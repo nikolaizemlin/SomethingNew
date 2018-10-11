eaad0a7a9f264ce1d73b6607d66065d6cb2b2d81
Nick Hosley <nhosley1976@gmail.com>
2018-10-10 19:15:06 -0700

    added more files

---
+++ server/routes/api/signin.js
@@ -0,0 +1,21 @@
+module.exports = (app) => {
+    // app.get('/api/counters', (req, res, next) => {
+    //   Counter.find()
+    //     .exec()
+    //     .then((counter) => res.json(counter))
+    //     .catch((err) => next(err));
+    // });
+  
+    // app.post('/api/counters', function (req, res, next) {
+    //   const counter = new Counter();
+  
+    //   counter.save()
+    //     .then(() => res.json(counter))
+    //     .catch((err) => next(err));
+    // });
+app.post('/api/account/signup', (req, res, next) => {
+    const { body }
+
+});
+
+};
